dfa0ffb24e23472712ef5226dd73a0d0df3352bd
yangguang <894304048@qq.com>
2024-08-13 14:27:22 +0800

    feat: add baidu webanalytics

--- src/components/head/WebAnalytics.tsx
+++ src/components/head/WebAnalytics.tsx
@@ -57,7 +57,6 @@ function BaiduAnalytics({ projectId }: { projectId: string }) {
       <script
         dangerouslySetInnerHTML={{
           __html: `
-      <script>
         var _hmt = _hmt || [];
         (function() {
           var hm = document.createElement("script");
@@ -65,7 +64,6 @@ function BaiduAnalytics({ projectId }: { projectId: string }) {
           var s = document.getElementsByTagName("script")[0]; 
           s.parentNode.insertBefore(hm, s);
         })();
-        </script>
       `,
         }}
       ></script>
